11a1b9e1d677342b1ff4b02d197a03367f3e64de
skxvtchy <jamesli784@gmail.com>
2023-05-01 12:42:54 -0400

    React Addition

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Main from "./components/Main";
+import Home from "./components/Home";
+// import Resume from "./components/Resume";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 
 export default function App() {
   return (
     <div className="container">
       <Navbar />
-      <Main />
+      <Home />
     </div>
   );
 }
---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import "./style.css";
+
+export default function Main() {
+  return (
+    <main>
+      {/* <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul> */}
+      <center>
+        <h1 class="name">James Li</h1>
+        <div class="links">
+          <a href="https://github.com/skxvtchy">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/jamesli784/">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+        <div class="about">
+          <p>Hi! I'm James.</p>
+          <p>
+            Currently I am a sophomore @ University at Buffalo pursuing a BS in
+            Computer Science
+          </p>
+          <p>
+            I've not had any real work experience as of yet but have created a
+            couple of side projects.
+          </p>
+          <p>
+            I am going to attend my first hackathon on Nov. 5th and will be
+            uploading a project called Recipe, focused on finding recipes for
+            leftover ingredients in your fridge
+          </p>
+        </div>
+        <br />
+
+        <h2 class="header">Projects</h2>
+        <div class="info">
+          <p>-Personal Portfolio</p>
+          <p></p>
+          <p>-RLE Encoder</p>
+          <p></p>
+          <p>-Solar Installation</p>
+          <p></p>
+        </div>
+        <br />
+        <h2 class="header">Current Coursework:</h2>
+        <div class="info">
+          <p>-Datastructures -Scala</p>
+          <p>-Systems -C</p>
+        </div>
+        <h2 class="header">Prog Languages:</h2>
+        <div class="info">
+          <p>-Html</p>
+          <p>-CSS</p>
+          <p>-Javascript</p>
+          <p>-Python</p>
+          <p>-Scala</p>
+          <p>-C</p>
+        </div>
+        <h2 class="header">Frameworks:</h2>
+        <div class="info">
+          <p>Empty</p>
+        </div>
+        <h2 class="header">Software:</h2>
+        <div class="info">
+          <p>Vscode</p>
+          <p>Intellij</p>
+          <p>Github</p>
+          <p>Photoshop</p>
+          <p>Excel</p>
+        </div>
+        <h2 class="header">Languages:</h2>
+        <div class="info">
+          <p>English //Fluent writing & speaking</p>
+          <p>Mandarin //Fluent speaking</p>
+          <p>Spanish //Limited Fluency</p>
+        </div>
+      </center>
+    </main>
+  );
+}
+
+// Hi! I'm James.
+// Currently I am a sophomore @ University at Buffalo pursuing a
+// BS in Computer Science
+// I've not had any real work experience as of yet but have created
+// a couple of side projects.
+// I am going to attend my first hackathon on Nov. 5th and will be
+// uploading a project called Recipe, focused on finding recipes for
+// leftover ingredients in your fridge
+// Current Coursework:
+// Datastructures -Scala
+// Systems -C
+// Skills:
+// Prog Languages:    Frameworks:         Software:       Languages:
+//     -Html              -Bootstrap          -Vscode           -English //fluent speaking & writing
+//     -Css               -React              -GitHub           -Mandarin //fluent speaking
+//     -Javascript        -jQuery             -Intellij         -Spanish //limited
+//     -Python                                -Photoshop
+//     -Scala                                 -Excel
+//     -C
+// Interests:
+// Stocks
+// Video Games
+// Skateboarding
+// //About
+// //portfolio
+// //resume
+// //contacts
+// //darkmode/lightmode
+// James Li 2022 | New York, United States
+// jamesli784@gmail.com
+// github linkedin
