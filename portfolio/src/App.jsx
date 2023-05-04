5c4942246ccafea3478700b1be1400134f4bbb3c
skxvtchy <jamesli784@gmail.com>
2023-05-04 13:44:53 -0400

    test

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-// import {SmoothScroll} from "./scripts.js";
-
-// import Resume from "./components/Resume";
-// import Projects from "./components/Projects";
-// import Contact from "./components/Contact";
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
