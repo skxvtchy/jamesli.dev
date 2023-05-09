4a637df525d97bd3ee33a069019394ad5d8ba4d3
skxvtchy <jamesli784@gmail.com>
2023-05-09 13:37:28 -0400

    mesh knot

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -18,20 +18,48 @@ export default function Main({ darkMode }) {
     <div className={darkMode ? "dark" : "light"}>
       <section id="About">
         <h1>About</h1>
-        <div className="box">
+        {/* <div
+          style={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="box"
+        >
           <Canvas
             camera={{ position: [0, 0, 40], fov: 20 }}
             alpha={true}
             gl={{ clearColor: "rgba(0, 0, 0, 0)" }}
+            scale={[15, 15, 15]}
           >
             <mesh rotation-y={Math.PI * 0.25} position-y={-3} scale={[5, 5, 5]}>
               <Experience />
             </mesh>
-          </Canvas>
-        </div>
+          </Canvas> 
+        </div> */}
+        <Canvas style={{ width: "100vw", height: "60vh" }}>
+          <mesh className="knot" scale={[1.5, 1.5, 1.5]}>
+            <torusKnotGeometry />
+            <meshNormalMaterial />
+          </mesh>
+        </Canvas>
+        <p>
+          Welcome to my personal website portfolio! My name is James, and I am a
+          student @ the University at Buffalo pursuing a Computer Science B.S. I
+          am currently seeking an internship opportunity, and this website
+          serves as a platform for me to showcase my skills, projects, and
+          experiences. On this website, you will find my resume, which outlines
+          my education, work experience, and relevant skills. You can also
+          explore my GitHub page, where I have uploaded some of my coding
+          projects. These projects demonstrate my ability to work with different
+          programming languages and frameworks, and showcase my problem-solving
+          and analytical skills.
+        </p>
         <div className="download">
-          <button type="button" className="resume">
-            <a href="/path/to/your/file.pdf" download>
+          <button type="button" className="click">
+            <a href="/path/to/your/resume.pdf" download>
               Resume
               <FontAwesomeIcon icon={faDownload} />
             </a>
