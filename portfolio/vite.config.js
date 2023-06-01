042f1e82427af159195cf5689d14a49d97cc0720
James Li <jamesli784@gmail.com>
2023-06-01 16:30:14 -0400

    chunk size bigger

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 500000,
     outDir: "build",
     rollupOptions: {
       input: {
